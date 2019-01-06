e503e99f2bbc2e30edf676dc534bd950b52a74ae
Deepak Sharma <deepaksharma040695@gmail.com>
2019-01-06 21:19:24 +0530

    internet receiver

---
+++ app/src/main/java/com/example/deepaksharma/androidcode/model/NetworkListenerBean.java
@@ -0,0 +1,13 @@
+package com.example.deepaksharma.androidcode.model;
+
+public class InternetChangeListenerBean {
+    private boolean isConnected;
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public void setConnected(boolean connected) {
+        isConnected = connected;
+    }
+}
