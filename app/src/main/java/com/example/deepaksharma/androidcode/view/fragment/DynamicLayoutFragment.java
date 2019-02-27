d70363347ee729df867f7e9e65baa0fc651f4631
Deepak <deepaksharma040695@gmail.com>
2019-02-27 23:46:23 +0530

    animation

--- app/src/main/java/com/example/deepaksharma/androidcode/view/fragment/DynamicLayoutFragment.java
+++ app/src/main/java/com/example/deepaksharma/androidcode/view/fragment/DynamicLayoutFragment.java
@@ -1,21 +1,12 @@
 package com.example.deepaksharma.androidcode.view.fragment;
 
-import android.content.Context;
 import android.databinding.ViewDataBinding;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.example.deepaksharma.androidcode.R;
 import com.example.deepaksharma.androidcode.databinding.FragmentDynamicLayoutBinding;
 import com.example.deepaksharma.androidcode.databinding.RowDynamicLayoutBinding;
-import com.example.deepaksharma.androidcode.databinding.RowTextListBinding;
 import com.example.deepaksharma.androidcode.utils.GlobalUtilities;
 import com.example.deepaksharma.androidcode.view.base.BaseFragment;
 import com.example.deepaksharma.androidcode.view.home.HomeActivity;
